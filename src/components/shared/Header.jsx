4025822aec353a7b864bddef425e53a0c89484de
janakripan <janakripanjkc@gmail.com>
2026-02-27 13:21:25 +0530

    changed header to glass surface

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -137,32 +137,20 @@ const Header = () => {
 
       {/*====== header navigaton bar======  */}
       <div className={`w-full h-fit px-16 transition-all duration-300 ease-in-out  ${showTopBar ? "pt-4 translate-y-0" : "pt-0 -translate-y-8"}`}>
-        {/* <GlassSurface
+        <GlassSurface
           width="100%"
           height="fit-content"
           borderRadius={8}
-          distortionScale={-30}
-          blur={5}
-          redOffset={0}
-          greenOffset={0}
-          blueOffset={0}
-          className=" bg-amber-200"
+          distortionScale={-180}
+          blur={25}
+           redOffset={0}
+  greenOffset={10}
+  blueOffset={20}
+          mixBlendMode="screen"
           
         >
          
-          <div className="w-full h-fit  flex flex-row items-center justify-between p-5.5 ">
-
-            left logo 
-
-            <div className="w-fit h-fit ">
-              <img src={logo} alt="logo image" className="w-12 h-auto " />
-
-            </div>
-            
-          </div>
-        </GlassSurface> */}
-
-        <div className="w-full relative h-fit flex flex-row justify-between items-center px-5 py-3 bg-white/10 rounded-lg backdrop-blur-lg border border-white/40 border-b-2 border-b-white/75 ">
+         <div className="w-full relative h-fit flex flex-row justify-between items-center px-5 py-3 bg-white/10 rounded-lg backdrop-blur-lg border border-white/40 border-b-2 border-b-white/75 ">
           {/* left logo  */}
 
           <div className="w-fit h-fit ">
@@ -204,6 +192,9 @@ const Header = () => {
             </button>
           </nav>
         </div>
+        </GlassSurface>
+
+        
       </div>
     </div>
   );
